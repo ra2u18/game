3844854dca76a8d2cf9adff713b0f6e6e768a8a5
ra2u181 <netlemon2021@gmail.com>
2022-08-03 18:03:33 +0300

    added monster support

---
+++ src/monster_ai_system.rs
@@ -0,0 +1,20 @@
+use specs::prelude::*;
+use super::{Viewshed, Position, Map, Monster};
+use rltk::{field_of_view, Point, console};
+
+pub struct MonsterAI {}
+
+impl<'a> System<'a> for MonsterAI {
+    type SystemData = ( ReadExpect<'a, Point>,
+                        ReadStorage<'a, Viewshed>,
+                        ReadStorage<'a, Monster> );
+    fn run(&mut self, data : Self::SystemData) {
+        let (player_pos, viewshed, monster) = data;
+
+        for(viewshed, _monster) in (&viewshed, &monster).join() {
+            if viewshed.visible_tiles.contains(&*player_pos) {
+                console::log(format!("Monster shouts insults"));
+            }
+        }
+    }
+}
